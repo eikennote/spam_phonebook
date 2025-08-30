908b8c79fcc506f79c343733936cbf4dcea0ccd6
eiken <hideyoshi.noda@outlook.jp>
2025-08-30 10:52:29 +0900

    add nabigation_bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :phone_numbers
   devise_for :users
+  root "phone_numbers#index"
   get "/", to: "phone_numbers#index"
   get "up" => "rails/health#show", as: :rails_health_check
 end
